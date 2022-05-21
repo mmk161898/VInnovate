a8306a4f311e2a9e87d2c081fe7d1c61ab9d1c39
meet <meetmevada1998@gmail.com>
2022-05-21 21:22:04 +0530

    New Project Created

---
+++ src/main/java/com/vinnovate/takehometest/TakeHomeTestApplication.java
@@ -0,0 +1,13 @@
+package com.vinnovate.takehometest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeHomeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeHomeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinnovate/takehometest/TakeHomeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinnovate.takehometest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeHomeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
